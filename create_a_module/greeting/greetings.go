97030da4db28bf3d81b6697677922e29175acb39
Nemya <yiduolnemya@gmail.com>
2025-01-04 16:10:27 -0500

    ch2: returning greetings for multiple people
    To start off, we would be changing the parameter for Hello() function to
    accomodate multiple names, which would change the function signature,
    and that would break people's code if this was a published module and
    people used the Hello() function.
    
    Instead a new function Hellos() is created, the function takes a slice
    of names and return a map of names (keys) paired with their greetings
    (values).
    
    You specify the type of the key within the bracket and the type of the
    value outside the bracket, ex: map[int]string.
    
    We loop through the slice with a for loop, note the slice is also a key,
    value data structure, where the index is the key, since the index is not
    needed in the loop, we use an underscore _ called blank identifier.
    
    The rest is simple.

--- create_a_module/greeting/greetings.go
+++ create_a_module/greeting/greetings.go
@@ -15,6 +15,21 @@ func Hello(name string) (string,error) {
 	message := fmt.Sprintf(randomFormat(),name)
 	return message,nil
 }
+// Hellos returns a map that associates each of the named people with a greeting message.
+func Hellos(names []string)(map[string]string,error)  {
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling the Hello function to get a message for each name.
+	// There is a blank operator here because the keys of names (index) are not needed, but the name value is.
+	for _,name := range names {
+		message,err := Hello(name)
+		if err!=nil {
+			return nil,err
+		}
+	// In the map, associate the retrieved message with the name.
+		messages[name]=message
+	}
+	return messages,nil
+}
 // randomFormat returns one of a set of greeting messages. The returned message is selected at random.
 func randomFormat() string {
 	// A slice of message formats.
--- create_a_module/hello/hello.go
+++ create_a_module/hello/hello.go
@@ -11,12 +11,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	// the log entry prefix and a flag to disable printing the time, source file, and line number.
     log.SetFlags(0)
-	// Request a greeting message.
-	message, err:=greeting.Hello("nemyA")
+	// A slice of names.
+	names := []string{"nemyA","Fool","Great Love","Nimueh","Aymons"}
+	// Request greeting messages for the names.
+	messages, err:=greeting.Hellos(names)
 	// If an error was returned, print it to the console and exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
-	// If no error was returned, print the returned message to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of messages to the console.
+	fmt.Println(messages)
 }
