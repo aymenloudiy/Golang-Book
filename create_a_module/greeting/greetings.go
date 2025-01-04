8d557aaef94e3facc9e0d2413d35b735f57f56af
Nemya <yiduolnemya@gmail.com>
2025-01-04 15:40:34 -0500

    ch2: add error handling to the greeting function
    the error is a variable that you create and return with the message.
    if there is an error you return in this case an empty message and a new
    error with errors.New() function.
    if there is no error you return the message and nil, idk what nil is but
    it's probably something like no error or null.
    in the hello function, if there is an error you should log it, using the
    appropriate logger.

--- create_a_module/greeting/greetings.go
+++ create_a_module/greeting/greetings.go
@@ -1,8 +1,16 @@
 package greeting
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string,error) {
+	// If there is no name given return an empty string and a new error
+	if name == "" {
+		return "",errors.New("empty name")
+	}
+	// If a name was received, return a value that embeds the name in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!" ,name)
-	return message
+	return message,nil
 }
--- create_a_module/hello/hello.go
+++ create_a_module/hello/hello.go
@@ -3,9 +3,20 @@ package main
 import (
 	"example/greeting"
 	"fmt"
+	"log"
 )
 
 func main() {
-	message:=greeting.Hello("nemyA")
+	// Set properties of the predefined Logger, including
+	log.SetPrefix("greetings: ")
+	// the log entry prefix and a flag to disable printing the time, source file, and line number.
+    log.SetFlags(0)
+	// Request a greeting message.
+	message, err:=greeting.Hello("")
+	// If an error was returned, print it to the console and exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message to the console.
 	fmt.Println(message)
 }
