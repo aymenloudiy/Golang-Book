94f108c26623b71a393e391196c055ccca1ceef8
Nemya <yiduolnemya@gmail.com>
2025-01-06 14:09:08 -0500

    ch3: multi-module workspaces
    Basically, we create a workspace in which multiple modules live.
    
    First we had the hello module that used a remote function from golang to
    reverse strings, it had to go download the whole golang.org/x/example
    and put it in a cache.
    
    We tell the workspace to use this module as a main module with
    go work init ./hello, now within our workspace we can run use ./hello to
    run the hello module, even if we are not inside the directory.
    
    Basically it initializes a Go workspace with the given module as the
    first module in that workspace.
    
    Next we clone the example repo and put it in the workspace as well.
    
    We add this module to the workspace using the go work use
    ./example/hello.
    
    Basically, this command is used to add an existing module to the
    workspace that has already been initialized with go work init.
    
    Then we modified the reverse file in the example/hello directory, and
    added a reverse integer function.
    
    Because we added example/hello to the workspace, the workspace will
    establish a link between golang.org/x/example/hello and the local
    example/hello directory, this ensures Go resolves the
    golang.org/x/example/hello import path to the local example/hello
    directory.

--- create_a_module/greeting/greetings.go
+++ create_a_module/greeting/greetings.go
@@ -6,38 +6,41 @@ import (
 	"math/rand"
 )
 
-func Hello(name string) (string,error) {
+func Hello(name string) (string, error) {
 	// If there is no name given return an empty string and a new error
 	if name == "" {
-		return "",errors.New("empty name")
+		return "", errors.New("empty name")
 	}
 	// If a name was received, return a value that embeds the name in a greeting message.
-	message := fmt.Sprintf(randomFormat(),name)
-	return message,nil
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
 }
+
 // Hellos returns a map that associates each of the named people with a greeting message.
-func Hellos(names []string)(map[string]string,error)  {
+func Hellos(names []string) (map[string]string, error) {
 	messages := make(map[string]string)
 	// Loop through the received slice of names, calling the Hello function to get a message for each name.
 	// There is a blank operator here because the keys of names (index) are not needed, but the name value is.
-	for _,name := range names {
-		message,err := Hello(name)
-		if err!=nil {
-			return nil,err
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
 		}
-	// In the map, associate the retrieved message with the name.
-		messages[name]=message
+		// In the map, associate the retrieved message with the name.
+		messages[name] = message
 	}
-	return messages,nil
+	return messages, nil
 }
+
 // randomFormat returns one of a set of greeting messages. The returned message is selected at random.
 func randomFormat() string {
 	// A slice of message formats.
-formats := []string{
-	"Hi, %v. Welcome!",
-	"Great to see you, %v!",
-	"Hail, %v! Well met!",
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	// Return a randomly selected message format by specifying a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
-    // Return a randomly selected message format by specifying a random index for the slice of formats.
-return formats[rand.Intn(len(formats))]
-}
+
